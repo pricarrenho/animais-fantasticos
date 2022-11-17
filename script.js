75cb48179416536f6abae7e2300f0a3651ddc430
Priscilla Carrenho <priscillaonil@gmail.com>
2022-11-17 15:58:42 -0300

    refactor: js improvements

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function initTabNav() {
-  const tabMenu = document.querySelectorAll(".js-tabmenu li");
-  const tabContent = document.querySelectorAll(".js-tabcontent section");
+  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
+  const tabContent = document.querySelectorAll('[data-tab="content"] section');
 
   if (tabMenu.length && tabContent.length) {
     tabContent[0].classList.add("ativo");
@@ -9,7 +9,8 @@ function initTabNav() {
       tabContent.forEach((section) => {
         section.classList.remove("ativo");
       });
-      tabContent[index].classList.add("ativo");
+      const direction = tabContent[index].dataset.anime;
+      tabContent[index].classList.add("ativo", direction);
     }
 
     tabMenu.forEach((itemMenu, index) => {
@@ -23,7 +24,9 @@ function initTabNav() {
 initTabNav();
 
 function initAccordion() {
-  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const accordionList = document.querySelectorAll(
+    '[data-anime="accordion"] dt'
+  );
 
   if (accordionList.length) {
     accordionList[0].classList.add("ativo");
@@ -43,7 +46,9 @@ function initAccordion() {
 initAccordion();
 
 function initScrollSuave() {
-  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  const linksInternos = document.querySelectorAll(
+    '[data-menu="suave"] a[href^="#"]'
+  );
 
   function scrollToSection(event) {
     event.preventDefault();
@@ -64,7 +69,7 @@ function initScrollSuave() {
 initScrollSuave();
 
 function initAnimacaoScroll() {
-  const sections = document.querySelectorAll(".js-scroll");
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
 
   if (sections.length) {
     const windowMetade = window.innerHeight * 0.6;
