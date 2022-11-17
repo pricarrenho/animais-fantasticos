69c4b748b0a25ba6a9e935fe950d1efdb7a4145a
Priscilla Carrenho <priscillaonil@gmail.com>
2022-11-17 18:22:37 -0300

    refactor: add modules js

---
+++ js/modules/scrip.js
@@ -0,0 +1,9 @@
+import initScrollSuave from "./scroll-suave.js";
+import initAnimacaoScroll from "./scroll-animacao.js";
+import initAccordion from "./accordion.js";
+import initTabNav from "./tabnav.js";
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
