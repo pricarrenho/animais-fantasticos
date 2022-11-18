eb021c03dfbcd1fb1b8483c35857476e86ffcf17
Priscilla Carrenho <priscillaonil@gmail.com>
2022-11-18 10:41:05 -0300

    feat: add tooltip

--- js/modules/scrip.js
+++ js/modules/scrip.js
@@ -3,9 +3,11 @@ import initAnimacaoScroll from "./scroll-animacao.js";
 import initAccordion from "./accordion.js";
 import initTabNav from "./tabnav.js";
 import initModal from "./modal.js";
+import initTooltip from "./tooltip.js";
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
 initModal();
+initTooltip();
